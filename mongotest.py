b3dc5e9822c000805dd02714cbde2130a676243a
yash-vij <yashvij1996@gmail.com>
2022-07-24 05:28:43 +0530

    My first git commit

---
+++ mongotest.py
@@ -0,0 +1,15 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://yashvij:0000@cluster0.wnq7l.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"yash vij",
+    "email" : "yashivj@gmail.com",
+    }
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
+
+
